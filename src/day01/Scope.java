993d58dee32191abd9a762f67f3f46ba590305e3
Youngsu Kim <126743125+Createcoding@users.noreply.github.com>
2023-03-27 11:08:34 +0900

    Merge branch 'master' into academydddss

---
+++ src/day01/Scope.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class Scope {
+
+    public static void main(String[] args) {
+
+        // 자바는 블록단위 스코프를 가짐
+        int n1 = 10;
+        int n2 = 10;    // main블록에서 생성된 변수
+
+
+    }
+}
